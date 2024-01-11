fa133c28d232aae85cd36079927e4185cd9844ed
asadbek000002 <asadbektuygunov9@gmail.com>
2024-01-11 15:42:32 +0500

    host1

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=798a&hf%harj%b_vmta2%4@-^ohea)%7=*(#(a01xb*lcezae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pythonanywhere.com', '127.0.0.1:8000']
 
 
 # Application definition
